735bab260c4effe8d42e6f0950d2205eec5954a6
com.korean <bibian1975@gmail.com>
2020-09-12 17:24:40 +0900

    Updates

---
+++ precache-manifest.b69ffee1f8dc41a26e9a5a3d9b07c35c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "619b520f75caca02341e4fd139e1d036",
+    "url": "/web.home/index.html"
+  },
+  {
+    "revision": "f3cb9bf2563bb912dfb8",
+    "url": "/web.home/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a036a0734b3aefc3f785",
+    "url": "/web.home/static/js/2.9b8f1604.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web.home/static/js/2.9b8f1604.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3cb9bf2563bb912dfb8",
+    "url": "/web.home/static/js/main.026a2ba8.chunk.js"
+  },
+  {
+    "revision": "f91d18663bb5139aebc2",
+    "url": "/web.home/static/js/runtime-main.ed121d81.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web.home/static/media/logo.5d5d9eef.svg"
+  }
+]);
